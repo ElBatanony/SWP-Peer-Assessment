4ed129cbd421ed6d85ca2d20bb935cfd071344a0
Ahmed ElBatanony <ahmedelbatanony@hotmail.com>
2019-03-19 12:07:07 +0300

    Some changes! Yay!
    Added utils page
    Added buttons to assignments page based on role
    Swapped subject and assignment name in solutions page
    Fixed id problem in solutions page
    Added checks for login in creation and details pages
    Added navbar to index page

---
+++ public/utils.js
@@ -0,0 +1,15 @@
+function gbid(x) {
+    return document.getElementById(x);
+}
+
+function goTo(x) {
+    window.location.href = x;
+}
+
+function signOut() {
+    firebase.auth().signOut().then(function () {
+        console.log('Signed Out');
+    }, function (error) {
+        console.error('Sign Out Error', error);
+    });
+}
