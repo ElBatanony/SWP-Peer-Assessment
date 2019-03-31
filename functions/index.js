93638ed9d162a8c69ef3e66afe4a1188a34c869d
Ahmed ElBatanony <ahmedelbatanony@hotmail.com>
2019-04-01 00:53:22 +0300

    Removed comments

--- functions/index.js
+++ functions/index.js
@@ -2,13 +2,6 @@ const functions = require('firebase-functions');
 var admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
 exports.modifyAssignment = functions.firestore
     .document('assignments/{assignmentID}')
     .onWrite((change, context) => {
@@ -22,8 +15,6 @@ exports.modifyAssignment = functions.firestore
         return db.collection("users").get().then(function (querySnapshot) {
             let promises = []
             querySnapshot.forEach(function (doc) {
-                // doc.data() is never undefined for query doc snapshots
-                //console.log(doc.id, " => ", doc.data());
                 let newnot = doc.data().notifications;
                 if (!newnot) newnot = [];
                 if ( newnot.indexOf(notificationMessage) == -1 ) newnot.push(notificationMessage);
