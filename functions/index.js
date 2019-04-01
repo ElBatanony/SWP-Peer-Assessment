d24a91e0d6f110db520eee5683496a209679da48
artyom-yurin <artem_yrin@mail.ru>
2019-04-01 23:02:03 +0300

    Hotfix

--- functions/index.js
+++ functions/index.js
@@ -50,7 +50,7 @@ exports.addUserDoc = functions.auth.user().onCreate((user) => {
 
 exports.getWork = functions.https.onCall((data, context) => {
         const assignemntID = data.assignmentID;
-        const uid = data.uid;//auth.uid
+        const uid = data.auth.uid;
 
         var db = admin.firestore();
         return db.collection("assignments").doc(assignemntID).get().then(function (doc) {
