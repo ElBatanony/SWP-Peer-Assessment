5073d56b90294554a47948437eb0833a588977a3
artyom-yurin <artem_yrin@mail.ru>
2019-04-01 22:45:06 +0300

    Added the distribution function

--- functions/index.js
+++ functions/index.js
@@ -25,3 +25,72 @@ exports.modifyAssignment = functions.firestore
         });
 
     });
+
+exports.addUserDoc = functions.auth.user().onCreate((user) => {
+    return admin.auth().getUser(user.uid)
+        .then(function (userRecord) {
+            return admin.firestore().collection("users").doc(user.uid).set({
+                name: userRecord.displayName,
+                email: user.email,
+                role: "student",
+                notifications: []
+            })
+            .then(function () {
+                console.log("Created a document for a new user " + user.uid);
+            })
+            .catch(function (error) {
+                console.error("Error writing document for a new user: ", error);
+            });
+        })
+        .catch(function (error) {
+            console.log("Error fetching user data:", error);
+        });
+    
+});
+
+exports.getWork = functions.https.onCall((data, context) => {
+        const assignemntID = data.assignmentID;
+        const uid = data.uid;//auth.uid
+
+        var db = admin.firestore();
+        return db.collection("assignments").doc(assignemntID).get().then(function (doc) {
+            if (doc.data().deadline > (new Date().getTime()))
+            {
+                return {message: "Assignment don't finished yet"};
+            } 
+            return db.collection("assignments").doc(assignemntID).collection("users").get().then(function (querySnapshot) {
+                done = [];
+                ref = "";
+                querySnapshot.forEach(function(doc)
+                {
+                    if(doc.id == uid)
+                    {
+                        done = doc.data().done;
+                        return;
+                    }
+                });
+                if(!done)
+                {
+                   done = [];
+                }
+                querySnapshot.forEach(function(doc)
+                {
+                    if(doc.id != uid && !(done.includes(doc.data().reference)))
+                    {
+                        ref = doc.data().reference;
+                        return;
+                    }
+                });
+                if(ref == "")
+                {
+                    return {
+                        message: "All works are checked"
+                    };
+    
+                }
+                return {
+                    message: ref
+                };
+            });
+        });
+    });
