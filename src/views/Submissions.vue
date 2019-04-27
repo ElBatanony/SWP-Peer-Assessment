72d3db6127b19535ebda62160f723361441cd650
Ahmed ElBatanony <ahmedelbatanony@hotmail.com>
2019-04-27 22:27:48 +0300

    Fixed Submissions.vue (I think)

--- src/views/Submissions.vue
+++ src/views/Submissions.vue
@@ -1,96 +1,59 @@
 <template>
   <div>
     <div v-if="assignment">
-        <h1 class="heading" :value="assignment.name">Loading...</h1>
-        <h2 class="subheading" value="assignment.subject">Loading...</h2>
-        <p class="subtitle" value="assignment.description">Loading...</p>
+        <h1 class="heading">{{assignment.name}}</h1>
+        <p class="subheading">{{assignment.description}}</p>
     </div>
     
     <h3 class="subtitle">List of submissions</h3>
-    <div id="submissions"></div>
+    <div v-for="submission in submissions" v-bind:key="submission.userId">
+        {{ submission.userId }}
+    </div>
   </div>    
 </template>
 
 <script>
 import { mapState, mapGetters } from "vuex";
+import firebase from 'firebase';
 
+var db = firebase.firestore();
 var app;
 
 export default {
     data: function () {
         return {
-            submissions: []
+            submissions: [],
+            assignment : null
         }
     },
   computed: {
     ...mapGetters(['isAdmin']),
     ...mapState([
-    'user','userDetails','assignments','db'
+    'user','userDetails','assignments'
     ])
   },
   created() {
-      this.assignment = null
+      app = this;
   },
   mounted() {
-    app = this;
     this.assignmentId = this.$route.params.assignmentId;
     this.assignment = this.assignments.filter( x => x.id == this.assignmentId )[0];
 
-    console.log( this.$route.params.assignmentId, this.assignment )
-
     if (!this.assignment) {
         console.log('no assignment')
-        return
+        this.$router.push('/assignments')
+        return;
     }
 
-    var codeError = this.$route.params.errorCode;
-
-    if (codeError) {
-        text = document.getElementById("errMessage");
-        if (codeError === "1") {
-            text.innerText = "Assignment don't finished yet";
-        } else if (codeError === "2") {
-            app.linkz = "";
-            text.innerText = "All works are checked";
-        } else if (codeError === "3") {
-
-            text.innerText = "No submit";
-        }
-    }    
-
-    this.GetReviews();
-  },
-  methods: {
-      GetReviews(){
-        var subm = document.getElementById("submissions");
-        app.db.collection("assignments").doc(app.assignmentId).collection("users").get().then(function(querySnapshot) {
-            querySnapshot.forEach(function(doc) {
-                // doc.data() is never undefined for query doc snapshots
-                app.db.collection("users").doc(doc.id).get().then(function(docU) {
-                    if (docU.exists) {
-                        subm.innerHTML += `<a href="/assessments/${app.assignmentId}/${doc.id}">Work by ${docU.data().name}</a><br>`
-                        //subm.innerHTML += "<a href='/assessmentsList?assignmentId=" + searchId + "&userId=" + doc.id + "'>Work by " + docU.data().name + "</a><br>"
-                    } else {
-                        // doc.data() will be undefined in this case
-                        console.log("No such document!");
-                        subm.innerHTML += `<a href="/assessments/${app.assignmentId}/${doc.id}">Work by anonymous</a><br>`
-                        //subm.innerHTML += "<a href='/assessmentsList?assignmentId=" + searchId + "&userId=" + doc.id + "'>Work by anonymous</a><br>"
-                    }
-                }).catch(function(error) {
-                    console.log("Error getting document:", error);
-                });
-            });
+    db.collection('submissions').where("assignmentId", "==", this.assignmentId).get()
+    .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            app.submissions.push( doc.data() )
         });
-      }
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
+    });
   }
 };
-
-/*
-    getDeadline: function () {
-        var date = new Date();
-        date.setTime(this.deadline);
-        parsedDate = date.toString().split(' ');
-        return parsedDate[0] + ' ' + parsedDate[1] + ' ' + parsedDate[2] + ' ' + parsedDate[3] + ' ' + parsedDate[4];
-    }
-*/
 </script>
