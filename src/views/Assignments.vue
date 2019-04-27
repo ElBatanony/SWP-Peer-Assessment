487a523eafcac78857fc6e57ef2500ae0ea93535
Ahmed ElBatanony <ahmedelbatanony@hotmail.com>
2019-04-27 22:37:43 +0300

    Fixed admin display in Assignments.vue

--- src/views/Assignments.vue
+++ src/views/Assignments.vue
@@ -6,13 +6,13 @@
     <div v-for="assignment in assignments" v-if="(assignment.course == userDetails.course || isAdmin)" v-bind:key="assignment.name">
         <span class="title">{{assignment.name}} </span>
         <v-chip color="info">{{assignment.course}}-{{assignment.subject}}</v-chip>
-        <v-chip v-bind:color="( (assignment.reviews||0) < (assignment.minreviews||0))? 'warning' : 'success'">{{assignment.reviews || 0}}/{{assignment.minreviews|| 0}} Reviews</v-chip>
-        <v-chip color="success" v-if="assignment.submitted">Submitted</v-chip>
-        <v-chip color="error" v-if="!assignment.submitted">Not Submitted</v-chip>
+        <v-chip v-if="!isAdmin" v-bind:color="( (assignment.reviews||0) < (assignment.minreviews||0))? 'warning' : 'success'">{{assignment.reviews || 0}}/{{assignment.minreviews|| 0}} Reviews</v-chip>
+        <v-chip color="success" v-if="!isAdmin && assignment.submitted">Submitted</v-chip>
+        <v-chip color="error" v-if="!isAdmin && !assignment.submitted">Not Submitted</v-chip>
 
         <v-btn small :to="'/submissions/'+assignment.id" v-if="isAdmin" class="warning">View Submissions</v-btn>
-        <v-btn small :to="'/assignments/edit/'+assignment.id" v-if="userDetails.role == 'admin'" class="warning">Edit Assignment</v-btn>
-        <v-btn small :to="'/assignments/'+assignment.id" v-if="userDetails.role != 'admin'" class="warning">View Assignment</v-btn>
+        <v-btn small :to="'/assignments/edit/'+assignment.id" v-if="isAdmin" class="warning">Edit Assignment</v-btn>
+        <v-btn small :to="'/assignments/'+assignment.id" v-if="!isAdmin" class="warning">View Assignment</v-btn>
     </div>
 
     <v-btn v-if="userDetails.role == 'admin'" class="info" v-on:click="createNewAssignment">Add Assignment</v-btn>
