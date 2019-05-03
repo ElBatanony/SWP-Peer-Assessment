415b26cc1c8a1b91fe2058186bd85a46d45368c1
Rim Rakhimov <rimrakhimov@gmail.com>
2019-05-03 12:08:27 +0300

    material design added

--- src/views/AssessmentReview.vue
+++ src/views/AssessmentReview.vue
@@ -1,14 +1,41 @@
 <template>
   <div>
-      
-    <h1 class="heading">Review assessment</h1>
-    <div v-for="(field, index) in fieldsArr" v-bind:key="index">
-        <v-text-field v-bind:label="field.name" v-model="field.value" readonly ></v-text-field>
-    </div>
+    
+    <v-layout justify-center>
+      <v-flex xs12 sm10 md8 lg6>
+        <v-card ref="form">
+          <v-toolbar color="success" dark>
+            <v-toolbar-title v-if="userName">{{userName}} submission</v-toolbar-title>
+          </v-toolbar>
 
-    <v-btn class="error" @click="deleteReview">Disapprove</v-btn>
-
-  </div>    
+          <!-- <v-list three-line>
+            <template v-for="(field, index) in fieldsArr">
+              <v-divider :key="index"></v-divider>
+              <v-list-item :key="field.name+field.description">
+                <v-list-item-content>
+                  <v-list-item-title>{{field.name}}</v-list-item-title>
+                  <v-list-item-subtitle></v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+            </template>
+          </v-list> -->
+          <v-card-text>
+            <div v-for="(field, index) in fieldsArr" v-bind:key="index">
+              <v-text-field v-if="field.type=='Text'" v-bind:label="field.name" v-model="field.value" readonly></v-text-field>
+              <v-textarea v-else v-bind:label="field.name" v-model="field.value" readonly></v-textarea>
+              <br>
+            </div>
+          </v-card-text>
+          <v-divider class="mt-5"></v-divider>
+          <v-card-actions>
+            <v-btn flat @click="$router.push('/assessments/' + getSubmissionId())">Go back</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn color="error" flat @click="deleteReview">Disapprove</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </div>
 </template>
 
 <script>
@@ -20,7 +47,9 @@ var app;
 export default {
     data : function () {
         return {
-            fieldsArr : []
+            fieldsArr : [],
+            userName: null,
+            submissionId: null
         }
     },
   computed: {
@@ -38,6 +67,8 @@ export default {
             app.fieldsArr = snapshot.data().fields;
             app.userId = snapshot.data().userId;
             app.assignmentId = snapshot.data().assignmentId;
+            app.userName = snapshot.data().username;
+            app.submissionId = snapshot.data().submissionId;
         } else {
             window.location.replace(window.location.origin + "/404");
         }
@@ -57,6 +88,10 @@ export default {
             }).catch(function (error) {
                 console.error("Error removing review: ", error);
             });
+    },
+    getSubmissionId() {
+      let id = app.submissionId.split("-");
+      return(id[0] + '/' + id[1]);
     }
   }
 };
